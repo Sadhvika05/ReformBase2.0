748836befebe9af440288766da241e8b425f2247
Sadhvika05 <sadhvikakonte30@gmail.com>
2024-09-05 12:35:56 +0530

    Add files via upload

---
+++ backend/models/Tables.js
@@ -0,0 +1,20 @@
+// models/Table.js
+
+const mongoose = require('mongoose');
+
+const TableSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Array,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Table', TableSchema);
